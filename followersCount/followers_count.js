b7b5c5a681dc45e33394d1f37d453b5d41852135
abdallah16 <guidabd@gmail.com>
2026-01-29 09:43:40 -0500

    followers counter

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -3,6 +3,7 @@ function increaseCount() {
     count++; // Increment the count by 1
     displayCount() // Display the count
     checkCountValue(); // Check count value and display messages
+    
 }
 
 function checkCountValue() {
@@ -17,3 +18,8 @@ function displayCount() {
    document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML    
 }  
 
+function resetCount(){
+    count=0;
+    document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML    
+    alert('Your followers have been reset')
+}
