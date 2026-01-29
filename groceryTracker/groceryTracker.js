650b4b739ad5ca678f44648f948fd95bb2119632
abdallah16 <guidabd@gmail.com>
2026-01-29 09:02:44 -0500

    grocery function project

---
+++ groceryTracker/groceryTracker.js
@@ -0,0 +1,9 @@
+function GroceryTracker(groc1,groc2,groc3){
+    groc1 = parseFloat(document.getElementById('grocery1').value);
+    groc2 = parseFloat(document.getElementById('grocery2').value);
+    groc3 = parseFloat(document.getElementById('grocery3').value);
+    let totalAmount= groc1+groc2+groc3
+
+    document.getElementById('result').innerText = `The total amount is : ${totalAmount}`;
+    
+}
